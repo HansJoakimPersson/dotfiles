0a395f142cd2dbb0e4101bd83bf935c0dadcc22a
Joakim Persson <hans.joakim.persson@gmail.com>
2020-02-08 18:52:17 +0100

    First commit

---
+++ my/bootstrap.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo "Setting up your Mac..."
+
+# pull in the latest version and copy the files to your home folder.
+cd "$(dirname "${BASH_SOURCE}")";
+git pull origin master;
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
+
+
