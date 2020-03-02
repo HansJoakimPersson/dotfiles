faeb014619fc1e7c02b824907ad4b9b897102016
Joakim Persson <joakim@MacBook-Pro.local>
2020-03-02 20:35:28 +0100

    Created the execute function

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2059
 
 ###############################################################################
 # Strict Mode
 ###############################################################################
-
 # Set strict mode -  via http://redsymbol.net/articles/unofficial-bash-strict-mode/
 # Return value of a pipeline is the value of the last (rightmost) command to
 # exit with a non-zero status, or zero if all commands in the pipeline exit
@@ -11,7 +11,7 @@
 
 # Print a helpful message if a pipeline with non-zero exit code causes the
 # script to exit as described above.
-trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+#trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 
 # Exit immediately if a pipeline returns non-zero.
 # Append "|| true" if you expect an error.
@@ -43,107 +43,138 @@ readonly _PROGAUTH="cert@polisen.se"
 readonly _PROGDATE=$(stat -f "%Sm" "$0" 2>/dev/null)          # for MacOS
 
 # Reset
-readonly reset="\x1b[0m" # Text Reset
+readonly col_reset="\x1b[0m" # Text Reset
 
 # Foreground color
-readonly green="\x1b[32m"
-readonly yellow="\x1b[33m"
+readonly col_green="\x1b[32m"
+readonly col_yellow="\x1b[33m"
+readonly col_red="\x1b[31m"
 
 # Special characters
 readonly char_succ="✔"
+readonly char_fail="✖"
 
-echo -e "Program: ${green}$_PROGFNAME${reset} by ${yellow}$_PROGAUTH${reset}"
-echo -e "Updated: ${green}$_PROGDATE${reset}\n"
+execute() {
+	printf '\n ⇒ %s ' "${@/eval/}"
+	if
+		("*@ > /dev/null 2>&1") &
+		spinner "$!"
+	then
+		ok
+	else
+		error
+	fi
+}
+
+spinner() {
+	local i sp n
+	sp="\|/-"
+	n=${#sp}
+	while ps a | awk '{print $1}' | grep -q "${1}"; do
+		sleep "0.75"
+		#printf "\b${sp:i++%${#sp}:1}"
+		printf "%s\b" "${sp:i++%n:1}"
+	done
+}
+error() {
+	printf '%s' "${col_red}${char_fail}${col_reset}"
+}
+
+ok() {
+	printf '%s' "${col_green}${char_succ}${col_reset}"
+}
 
 ###############################################################################
 # Main script
 ###############################################################################
 
-echo -e '=============================================='
-echo -e '=           ${yellow}Setting up your Mac...${reset}           ='
-echo -e '=============================================='
+printf "================================================================================\n"
+printf "=                            Setting up your Mac...                            =\n"
+printf "================================================================================\n"
+printf "Program: ${col_green}$_PROGFNAME${col_reset} by ${col_yellow}$_PROGAUTH${col_reset}\n"
+printf "Updated: ${col_green}$_PROGDATE${col_reset}\n"
 
 # Revoke any previous sudo access
 sudo -k
 
 # Ask for the administrator password upfront
-echo
-echo -n 'Some settings require sudo to change. '
+printf "\nSome settings require sudo to change. "
 sudo -v
 
 # Please give me some answers
-read  -p "Change hostname for this computer (Enter to skip): " hostname
-read  -p "Adding dotfiles to home folder may overwrite existing files. Are you sure? (y/n) " dotfiles
+read  -p -r "Change hostname for ${HOSTNAME}: (Enter to skip):"  hostname
+read  -p -r "Adding files to home folder may overwrite existing files. Are you sure? (y/N) " dotfiles
 
-echo
-echo "From this point on it should be an automated process,\n ☕☕☕ go grab a coffee! ☕☕☕\n\n"
+clear
+
+# Coffee art
+printf "$(base64 --decode <<<'H4sIAKGEWV4A/7VYPZPiMAzt7y9ccz8BloXdHUom5c4wTLrb6vpcfT9/rTiA9fRlxzkXMBDHfnqSniX/yuPn17/9n9+vH9PyW4w84fB+Pp5fDsfpmkf4Gi6wO++mr79Vm+KOz9kKlgYgK8DIDR0w5UK2DZuD9DA2PmxB+V9B3kctoF4wrl/ZqEDUHWPaiPbtzjJrO3VegKYZTIXO+E6LHm8EUsfpJX70dFuQdbG0kfucyFj+PN2Fb/n9Nn1eij+O79NlGIo/Pl6nWxrjDSbxKePIJszLihn2CgQL5t/SC2PamL9UIqNNcBFumynyVWxaYaNunhCXj/eHHTNo//YyDQMROY9hSCSXttB8tn5+AWZcrrjmY8W0po0G6TWZqeHFpWW2Gz2JPkzWI5McXUILZMu8QcczajD+clRjXA8lMNVjasRuQJmdCQ+nlv+dTnwOQCVvM/eL7OJsZIJdfpHdiFwk1s0FNEcxmQTD14fy8bwDW1LZ4uJOeGIsjq4ur7Lk5amaFQCT+/KZBkdJsAvZEIFBrzkxi8+JNkdQyXOMVlqOBdYsU73coHRKcrwBcc4PBPXE49GajdJJ6hG+edkVNsjklFW/Lt1mCKyyznUZ5lu9x/wD2zWVwjEQaDz0FpAdlqLSNPjVky9hqBehWlYblj6r8J5ihg7pNe7lzlWqLz8VsWYitRNyAyfN8rhapGvqlRWmO0tghiIxUnTDMsTjRJR6So7Jdqe9kQgltSZeZEGLZUxj1bq6LWo5qxsDIKy+RAva2FhunctuMGtZ3dFUhR1qM369PHE1SW25I/gtJc1cA4+j7Iyg1RbVy3oi7NNHEZzGZqGKQ5c/J93IZ7x453ISHKt+K2XKkuMIs9iJ1Mu5IrFZNGkrOgDrzMV4h4rJEVAVlNJV1fDpNxwOKbJtA2ubhamdzobI15rOoPnCm5DgBiy8wkCJqWiK8v1d5FfKiGW5wAZHWcwgr5UGpeK/MlJYCLYcwXCFoZCZ5Frcx91HbszpM32JlBAhjUETud1S1/oSv6Wj0eKDX85m45OxaAjdx/JbtHydJy7WHncWbreu3PsFt34rCtna+/e6O/ly0x/fsxsVXa8dAAA=' | gunzip)"
+printf "\n  From this point on it should be an automated process, go grab a coffee! "
+
+secs=$((5))
+while [ $secs -gt 0 ]; do
+	#echo -ne "$secs\033[0K\r"
+	printf "%s\b" "$secs"
+	sleep 1
+	: $((secs--))
+done
+printf " \b"
 
 # If we're using APFS, make a snapshot before doing anything.
-if   [[ -n "$(  mount | grep '/ (apfs')" ]]; then
-	echo "Taking local APFS snappshot\n"
-	sudo tmutil localsnapshot
-	echo -e "Done ${green}${char_succ}${reset}"
+if    [[ -n "$(  mount | grep '/ (apfs')" ]]; then
+	printf "\n\n${col_yellow}Taking local APFS snappshot${col_reset} "
+	#execute eval "sudo tmutil localsnapshot"
 fi
 
+# disable sleep
+printf "\n\n${col_yellow}Temporary disabling sleep${col_reset}"
+execute eval "sudo pmset -a sleep 0"
+execute eval "sudo pmset -a hibernatemode 0"
+execute eval "sudo pmset -a disablesleep 1"
+exit
 # Set hostname
 if  [[ -n $hostname     ]]; then
 	#Set computer name (as done via System Preferences → Sharing)
-	echo "Setting hostname to $hostname"
-	sudo scutil --set ComputerName "$hostname" &&
-		sudo scutil --set HostName "$hostname" &&
-		sudo scutil --set LocalHostName "$hostname" &&
-		sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$hostname"
-	echo -e "Done ${green}${char_succ}${reset}"
+	printf "\n\n${col_yellow}Setting hostname to${col_reset} $hostname\n"
+	execute eval "sudo scutil --set ComputerName ${hostname} &&"
+	execute eval "sudo scutil --set HostName ${hostname} &&"
+	execute eval "sudo scutil --set LocalHostName ${hostname} &&"
+	execute eval "sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string ${hostname}"
+
 fi
 
-echo -e '=============================================='
-echo -e '=            ${yellow}Adding prerequisites${reset}            ='
-echo -e '=============================================='
-
-# disable sleep
-echo "Temporary disabling sleep"
-sudo pmset -a sleep 0
-sudo pmset -a hibernatemode 0
-sudo pmset -a disablesleep 1
-
 # Check for Homebrew and install if we don't have it
-echo "Checking for Homebrew"
-if test ! "$(which brew)"; then
-	echo "Downloading and installing Homebrew"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	echo -e "Done ${green}${char_succ}${reset}"
+printf "\n\n${col_yellow}Checking homebrew${col_reset} "
+if test ! "$(command -v brew)"; then
+	printf "\n ⇒  downloading and installing Homebrew "
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew analytics off
+else
+	ok
 fi
 
 # Check if this already is a git repository of my dot files
 if git remote -v 2>/dev/null  | grep -q 'https://github.com/HansJoakimPersson/dotfiles'; then
-	echo "Pull in the latest version of this git repository"
-	# cd "$(dirname "${BASH_SOURCE}")" || exit
-	git pull origin master
-	echo -e "Done ${green}${char_succ}${reset}"
+	printf "\n\n${col_yellow}Pull in the latest version of this git repository${col_reset}"
+	execute eval "git pull origin master"
 else
-	echo "Clone the latest version of this git repository"
-	# cd "$(dirname "${BASH_SOURCE}")" || exit
-	git clone https://github.com/HansJoakimPersson/dotfiles
+	printf "\n\n${col_yellow}Clone the latest version of this git repository${col_reset}"
+	execute eval "git clone https://github.com/HansJoakimPersson/dotfiles"
 	cd dotfiles
-	echo -e "Done ${green}${char_succ}${reset}"
 fi
 
 # Install packages from homebrew
-echo -e '=============================================='
-echo -e '=        ${yellow}Installing Homebrew packages${reset}        ='
-echo -e '=============================================='
+printf "\n\n${col_yellow}Installing Homebrew packages${col_reset}"
 sh brew
-echo -e "Done ${green}${char_succ}${reset}"
 
 # Set up MacOS
-echo -e '=============================================='
-echo -e '=           ${yellow}Setting macOS defaults${reset}           ='
-echo -e '=============================================='
+printf "\n\n${col_yellow}Updating system configuration${col_reset}"
 sh macos
-echo -e "Done ${green}${char_succ}${reset}"
 
 # Adding dotfiles to home folder
-function doIt() {
+if  [[ $dotfiles =~ ^[y|yes|Y]$   ]]; then
+	printf "\n\n${col_yellow}Adding dotfiles to home folder${col_reset}\n ⇒ "
+
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
@@ -151,21 +182,18 @@ function doIt() {
 		--exclude "brew" \
 		--exclude "macos" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~
-	source ~/.bash_profile
-}
+		--filter="+ .*" \
+		--filter="- *" \
+		-avh --no-perms -c --backup --backup-dir="backup_$(date +"%Y-%m-%d %H.%M.%S")" . ~/dofiles
 
-if  [[ $dotfiles =~ ^[Yy]$   ]]; then
-	doIt
+	printf "\n${col_yellow}Loading new .bash_profile${col_reset}"
+	execute eval "source ~/.bash_profile"
 fi
-unset doIt
 
-echo -e "Done ${green}${char_succ}${reset}"
+# reenabling sleep
+printf "\n${col_yellow}Reenabling sleep${col_reset}"
+execute eval "sudo pmset -a sleep 1"
+execute eval "sudo pmset -a hibernatemode 3"
+execute eval "sudo pmset -a disablesleep 0"
 
-# disable sleep
-echo "Reenabling sleep"
-sudo pmset -a sleep 1
-sudo pmset -a hibernatemode 3
-sudo pmset -a disablesleep 0
-
-echo -e "All done ${green}${char_succ}${reset}, you should now reboot"
+printf "\n\nAll done, you should now reboot!\n\n"
